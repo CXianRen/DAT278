66c459d435bd6b35cebc48fc3552c451755bbc71
hongguang.chen <chenhon@chalmers.se>
2023-11-27 16:21:57 +0100

    update

--- Lab3/lab3/power_tools/energy_parser.py
+++ Lab3/lab3/power_tools/energy_parser.py
@@ -1,28 +1,58 @@
+import sys
+import numpy
 import itertools
 
-file_ptr = open("/tmp/RAPL_Energy_0.txt", "r+")
+import pandas
 
-avg_cpupower = 0.0
-total_cpuenergy = 0.0
+csv = pandas.read_csv("./../sparsity/layer1.txt")
+def check(start,end):
+    filePtr = open("/tmp/RAPL_Energy_0.txt", "r+")
+    i = 0
+    AVE_cpupower = 0.0
+    total_energy = 0.0
+    for line in itertools.islice(filePtr, 1, None):
+        token    = line.strip().split(',')
+        time     = numpy.int64(token[0])
+        energy   = float(token[1])
+        cpupower = float(token[2])
 
-i = 0
+        if time >= start and time <= end:
+            if i == 0:
+                energy_old = energy
+            if energy >= energy_old:
+                total_energy += energy - energy_old
+                AVE_cpupower += cpupower
+                i += 1
+            else:
+                total_energy += energy
+            energy_old = energy
+        elif time > end:
+            break
+        else:
+            continue
+    filePtr.close()
+    
+    return (AVE_cpupower/i), (total_energy/1000000)
+    #print(" Avg. CPU power : %.4f W\nTotal CPU energy: %.4f J" % (AVE_cpupower/i, total_energy/1000000))
 
-for line in itertools.islice(file_ptr, 1, None):
 
-    token    = line.strip().split(',')
-    energy   = float(token[1])
-    cpupower = float(token[2])
-
-    if i == 0:
-        energy_old = energy
-    if energy >= energy_old:
-        total_cpuenergy += energy - energy_old
-        avg_cpupower += cpupower
-        i += 1
-    else:
-        total_cpuenergy += energy
-    energy_old = energy
-
-print(" Avg. CPU power : %.4f W\nTotal CPU energy: %.4f J" % (avg_cpupower/i, total_cpuenergy/1000000))
-
-file_ptr.close()
+for i in range(10):
+    print("f,", csv.iloc[i][0], end=",")
+    # dense
+    start = csv.iloc[i][2]
+    end = csv.iloc[i][3]
+    # print(start, end)
+    p,e = check(start,end)
+    print(p,e,sep=',', end=",")
+    # coo
+    start = csv.iloc[i][6]
+    end = csv.iloc[i][7]
+    # print(start, end) 
+    p,e = check(start,end)
+    print(p,e,sep=',', end=",")
+    # CSR
+    start = csv.iloc[i][10]
+    end = csv.iloc[i][11]
+    # print(start, end)
+    p,e = check(start,end)
+    print(p,e,sep=',')
--- Lab3/lab3/sparsity/sparsity_auto.py
+++ Lab3/lab3/sparsity/sparsity_auto.py
@@ -6,6 +6,8 @@ import time
 from scipy.sparse import rand
 from scipy.sparse import coo_matrix, csc_matrix, csr_matrix
 
+import subprocess
+
 LAYER_SIZES = [
     {'M' :   64, 'N' : 3025, 'K' :  363}, # 1
     {'M' :  192, 'N' :  729, 'K' : 1600}, # 2
@@ -21,7 +23,7 @@ DENSITIES = [0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0.01]
 if __name__ == '__main__':
 
     # Generate a MxN matrix with density 'd' (in COO format)
-    layer = LAYER_SIZES[6]
+    layer = LAYER_SIZES[0]
     M = layer['M']
     N = layer['N']
     K = layer['K']
@@ -36,41 +38,48 @@ if __name__ == '__main__':
         return int(start * 1000),int(  end * 1000),int((end - start) * 1000)/it
 
     def Dense_size(A,B,C):
-        return (A.size+B.size+C.size)*8
+        return A.data.nbytes + B.data.nbytes+ C.data.nbytes
     
     def COO_size(A,B,C):
-        return (A.data.size+B.data.size+C.data.size)*(8+4+4)
+        return (A.data.nbytes+B.data.nbytes+C.data.nbytes)+\
+                (A.row.nbytes+B.row.nbytes+C.row.nbytes) +\
+                (A.col.nbytes+B.col.nbytes+C.col.nbytes)
     
     def CSR_size(A,B,C):
         return (A.data.size+B.data.size+C.data.size)*(8+4) + (A.indptr.size+ B.indptr.size + C.indptr.size)*4 
-
     print("densities,ds,D_start,D_end,D_dt,COO_s,COO_start,COO_end,COO_dt,CSR_s,CSR_start,CSR_end,CSR_dt")
     for d in DENSITIES:
         A = rand(M, K, density=d)
         B = rand(K, N, density=d)
         C = rand(M, N, density=d)
         coo_s = COO_size(A,B,C)
-        
+    
         # Convert matrix
         A_d = A.todense()
         B_d = B.todense()
         C_d = C.todense()
         d_s = Dense_size(A_d,B_d,C_d)
         
+        
         A_csr = A.tocsr()
         B_csr = B.tocsr()
         C_csr = C.tocsr()
         csr_s = CSR_size(A_csr,B_csr,C_csr)
-
-        it = 10
+        
+        it = 50
         # Dense
+        time.sleep(1)
         start_d,end_d,time_ms_d = test(A_d,B_d,C_d,it)
         # COO
+        time.sleep(1)
         start_coo,end_coo,time_ms_coo = test(A,B,C,it)
         # CSR
+        time.sleep(1)
         start_csr,end_csr,time_ms_csr = test(A_csr,B_csr,C_csr,it)
+        time.sleep(1)
         print(d,
               d_s,start_d,end_d,time_ms_d,
               coo_s,start_coo,end_coo,time_ms_coo,
               csr_s,start_csr,end_csr,time_ms_csr,sep=',')
+    
     
